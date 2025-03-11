8f0fd231f4faf01d5ec9fb89d145f291fa5a3f09
Maes95 <michel.maes95@gmail.com>
2025-03-11 21:34:24 +0100

    Add base project for ejer2

---
+++ atdd/atdd_ejer2_enunciado/src/test/java/es/codeurjc/test/cucumber/ComplexAddRunSteps.java
@@ -0,0 +1,12 @@
+package es.codeurjc.test.cucumber;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ComplexAddRunSteps {
+
+}
